c39eeb0eb51ecc8f6d21b869830ed30595467524
Austin Lyksett <alyksett1029@gmail.com>
2025-04-14 21:38:51 -0500

    Remove dist files

--- dist/values.ts
+++
@@ -1,4 +0,0 @@
-export const PEER_ID = Math.random().toString(36);
-export const SIGNING_KEY = Math.random().toString(36);
-export const CLUSTER_ID = "Math.random().toString(36)";
-export const SHARED_KEY = "Math.random().toString(36)";
